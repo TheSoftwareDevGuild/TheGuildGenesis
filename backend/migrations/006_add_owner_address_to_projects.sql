488eee77fd3f97522bda29d2314a3dbc367ae38d
Ifeoluwa <pheobeayo@gmail.com>
2026-03-18 07:48:38 +0100

    fix CI issues

--- backend/migrations/006_add_owner_address_to_projects.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE projects
-    ADD COLUMN IF NOT EXISTS owner_address VARCHAR(42)
-        GENERATED ALWAYS AS (creator) STORED;
-
-CREATE INDEX IF NOT EXISTS idx_projects_owner_address ON projects(owner_address);
